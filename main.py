e7ae53edd8e3de19e304ac73e052f7d2143fe4f5
anadev <computador.ana27@gmail.com>
2025-10-24 09:24:43 -0300

    alterei o menu

--- main.py
+++ main.py
@@ -19,12 +19,73 @@ while True:
         option = str(input("Escolha uma opção no menu acima: "))
 
         if option == "1":
-            Animal.adicionar_animal()
+            print("\n===📋 Adicionar pet 📋===")
+            nome = input("Nome: ")
+            idade = input("Idade: ")
+            dono = input("Dono: ")
+
+            resultado = Animal.adicionar_animal(nome, idade, dono)
+            print(f"\n Animal cadastrado com sucesso!" if resultado == True else resultado)
+
         elif option == "2":
-            remover_animal()
+            print("\n===❌Remover pet❌===")
+            nome = input("insira o nome para a remoção do pet: ")
+
+            resultado = Animal.remover_animal(nome)
+            print(f"Animal removido com sucesso!" if resultado == True else "Não foi possível remover o Animal!")
+
         elif option == "3":
-            buscar_animal()
+            print("\n===🔎Buscar pet🔍===")
+            nome = input("insira o nome para buscar o pet: ")
+            resultado = Animal.buscar_animal(nome)
+            if not resultado:
+                print("\nPet não encontrado!")
+            else:
+                print(f"\nNome: {resultado[0]} | CPF: {resultado[1]}")
+
         elif option == "4":
-            exibir_animais()
+            print("\n===📑Exibir Usuários📑===")
+            biblioteca.exibir_usuarios()
+
         elif option == "5":
+            print("\n===📋 Adição de Livro 📋===")
+            titulo = input("Título: ")
+            ano = input("Ano: ")
+            autor = input("Autor: ")
+            genero = input("Gênero: ")
+            resultado = biblioteca.adicionar_livro(titulo, ano, autor, genero)
+            print("\nLivro adicionado com sucesso!" if resultado == True else resultado)
+
+        elif option == "6":
+            print("\n===❌Remoção de Livro❌===")
+            titulo = input("insira o título para a remoção do livro: ")
+            resultado = biblioteca.remover_livro(titulo)
+            print(f"Livro removido com sucesso!" if resultado == True else "Não foi possível remover o Livro!")
+
+        elif option == "7":
+            print("\n===🔎Buscar Usuário🔍===")
+            titulo = input("insira o título para buscar o livro: ")
+            resultado = biblioteca.buscar_livro(titulo)
+            if not resultado:
+                print("\nLivro não encontrado!")
+            else:
+                print(f"titulo: {resultado[0]} | autor: {resultado[1]} | ano: {resultado[2]} | genero: {resultado[3]} | disponivel: {resultado[4]}")
+
+        elif option == "8":
+            print("\n===📖Exibir Livros📖===")
+            biblioteca.exibir_livros()
+
+ 
+        
+        elif option == "13":
+            biblioteca.salvar_usuarios()
+            biblioteca.salvar_livros()
+            print("Saindo do programa...")
+            break
+        else:
+            print("Opção inválida! Tente novamente.")
+
+if __name__ == "__main__":
+
+    menu()
             
