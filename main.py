0add2ac1f1b97a2a0449fabf15c9545064711514
ana clara <computador.ana27@gmail.com>
2025-10-25 10:26:23 -0300

    Alterei o menu

--- main.py
+++ main.py
@@ -1,91 +1,54 @@
 from animal import Animal, Cachorro, Gato
 
-    
-while True:
+def menu():
+    while True:
         print("""
         #========🐾 Menu Principal 🐕‍🦺========#
         1️⃣ - Adicionar animal 
         2️⃣ - Remover animal
         3️⃣ - Buscar animal
         4️⃣ - Exibir animais
-        5️⃣ - Adicionar dono
-        6️⃣ - Remover dono
-        7️⃣ - Buscar dono
-        8️⃣ - Exibir donos
         9️⃣ - Sair
         #====================================#
         """)
         
-        option = str(input("Escolha uma opção no menu acima: "))
+        option = input("Escolha uma opção no menu acima: ")
 
         if option == "1":
-            print("\n===📋 Adicionar pet 📋===")
+            print("\n===📋 Adicionar Pet 📋===")
             nome = input("Nome: ")
             idade = input("Idade: ")
             dono = input("Dono: ")
 
             resultado = Animal.adicionar_animal(nome, idade, dono)
-            print(f"\n Animal cadastrado com sucesso!" if resultado == True else resultado)
+            print("\nAnimal cadastrado com sucesso!" if resultado else "Erro ao cadastrar!")
 
         elif option == "2":
-            print("\n===❌Remover pet❌===")
-            nome = input("insira o nome para a remoção do pet: ")
+            print("\n===❌ Remover Pet ❌===")
+            nome = input("Nome do pet para remover: ")
 
             resultado = Animal.remover_animal(nome)
-            print(f"Animal removido com sucesso!" if resultado == True else "Não foi possível remover o Animal!")
+            print("Animal removido com sucesso!" if resultado else "Pet não encontrado!")
 
         elif option == "3":
-            print("\n===🔎Buscar pet🔍===")
-            nome = input("insira o nome para buscar o pet: ")
+            print("\n===🔎 Buscar Pet 🔍===")
+            nome = input("Nome do pet para buscar: ")
             resultado = Animal.buscar_animal(nome)
             if not resultado:
-                print("\nPet não encontrado!")
+                print("Pet não encontrado!")
             else:
-                print(f"\nNome: {resultado[0]} | CPF: {resultado[1]}")
+                print(f"\nNome: {resultado[0]} | Idade: {resultado[1]} | Dono: {resultado[2]}")
 
         elif option == "4":
-            print("\n===📑Exibir Usuários📑===")
-            biblioteca.exibir_usuarios()
+            print("\n===📑 Lista de Pets 📑===")
+            Animal.exibir_animais()
 
-        elif option == "5":
-            print("\n===📋 Adição de Livro 📋===")
-            titulo = input("Título: ")
-            ano = input("Ano: ")
-            autor = input("Autor: ")
-            genero = input("Gênero: ")
-            resultado = biblioteca.adicionar_livro(titulo, ano, autor, genero)
-            print("\nLivro adicionado com sucesso!" if resultado == True else resultado)
-
-        elif option == "6":
-            print("\n===❌Remoção de Livro❌===")
-            titulo = input("insira o título para a remoção do livro: ")
-            resultado = biblioteca.remover_livro(titulo)
-            print(f"Livro removido com sucesso!" if resultado == True else "Não foi possível remover o Livro!")
-
-        elif option == "7":
-            print("\n===🔎Buscar Usuário🔍===")
-            titulo = input("insira o título para buscar o livro: ")
-            resultado = biblioteca.buscar_livro(titulo)
-            if not resultado:
-                print("\nLivro não encontrado!")
-            else:
-                print(f"titulo: {resultado[0]} | autor: {resultado[1]} | ano: {resultado[2]} | genero: {resultado[3]} | disponivel: {resultado[4]}")
-
-        elif option == "8":
-            print("\n===📖Exibir Livros📖===")
-            biblioteca.exibir_livros()
-
- 
-        
-        elif option == "13":
-            biblioteca.salvar_usuarios()
-            biblioteca.salvar_livros()
-            print("Saindo do programa...")
+        elif option == "9":
+            print("Saindo do sistema... Até logo! 🐾")
             break
+
         else:
-            print("Opção inválida! Tente novamente.")
+            print("⚠ Opção inválida! Tente novamente.")
 
 if __name__ == "__main__":
-
     menu()
-            
